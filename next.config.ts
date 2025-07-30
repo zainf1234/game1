09e9ca2cf3e8b4dc4770c0edcf1ab74c8fdb2019
zainf1234 <zainfecto@gmail.com>
2025-07-30 12:04:11 -0400

    f

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/game1', // replace with your GitHub repo name
+  trailingSlash: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
