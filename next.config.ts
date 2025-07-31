e1430570384c83803e94705c058eca0a1a227d64
zainf1234 <zainfecto@gmail.com>
2025-07-31 10:33:56 -0400

    d

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/game1', // replace with your GitHub repo name
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
